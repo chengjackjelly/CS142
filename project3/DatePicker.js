858c2f7af2bc0b4beeee41e33146413caa329a63
“jack” <601427268@qq.com>
2023-09-17 16:42:19 +0200

    project3 datepicker by myself

--- project3/DatePicker.js
+++ project3/DatePicker.js
@@ -54,13 +54,13 @@ class DatePicker {
         this.year_month = document.createElement("div");
         this.year_month.className = "year-month";
         this.bt_left = document.createElement("button");
-        this.bt_left.className = "bt";
+        this.bt_left.className = "lbt";
         this.bt_left.textContent = "<";
         this.year_month_text = document.createElement("p");
         this.year_month_text.className = "year-month-text";
         this.bt_right = document.createElement("button");
         this.bt_right.textContent = ">";
-        this.bt_right.className = "bt";
+        this.bt_right.className = "rbt";
         this.year_month.appendChild(this.bt_left);
         this.year_month.appendChild(this.year_month_text);
         this.year_month.appendChild(this.bt_right);
@@ -78,10 +78,8 @@ class DatePicker {
         this.datapicker.appendChild(this.grid_calendar);
 
     }
-
-    render(date) {
+    setDate(date) {
         var content = document.getElementById(this.divId);
-        content.appendChild(this.datapicker);
 
         var month = date.getMonth() + 1;
         var thedate = date.getDate();
@@ -105,8 +103,87 @@ class DatePicker {
             var element = document.createElement("div");
             element.className = "grid-day";
             element.textContent = i;
+            element.year = date.getFullYear();
+            element.month = date.getMonth();
+            element.day = date.getDate();
+            element.curDate = this.curDate;
+            element.divId = this.divId;
             parent.append(element);
+            element.callback = this.callback;
+            element.addEventListener("click", function (event) {
+                if (this.year === this.curDate.getFullYear() && this.month === this.curDate.getMonth()) {
+                    this.callback(this.divId, this);
+                }
+
+            })
         }
+
+    }
+    render(date) {
+        this.curDate = date;
+        var content = document.getElementById(this.divId);
+        content.appendChild(this.datapicker);
+
+        this.setDate(date);
+        var year_month = content.querySelector("div.datapicker").querySelector("div.year-month");
+        var lbt = year_month.querySelector("button.lbt");
+        lbt.divId = this.divId;
+        lbt.setDate = this.setDate;
+        lbt.date = date;
+        var rbt = year_month.querySelector("button.rbt");
+        rbt.divId = this.divId;
+        rbt.setDate = this.setDate;
+        rbt.date = date;
+        lbt.partner = rbt;
+        rbt.partner = lbt;
+        lbt.callback = this.callback;
+        rbt.callback = this.callback;
+        lbt.curDate = this.curDate;
+        rbt.curDate = this.curDate;
+        lbt.addEventListener("click", function (event) {
+            var content = document.getElementById(this.divId);
+            var year_month_text = content.querySelector("div.datapicker").querySelector("div.year-month").querySelector("p.year-month-text");
+            year_month_text.textContent = "";
+
+            const elements = content.querySelector("div.datapicker").querySelector("div.grid-calendar").querySelectorAll(".grid-day");;
+            for (var i = 0; i < elements.length; i++) {
+                var elem = elements[i];
+                elem.parentNode.removeChild(elem);
+            }
+
+            const elementsDim = content.querySelector("div.datapicker").querySelector("div.grid-calendar").querySelectorAll(".grid-day-dimmed");
+            for (var i = 0; i < elementsDim.length; i++) {
+                var elem = elementsDim[i];
+                elem.parentNode.removeChild(elem);
+            }
+            this.date = new Date(this.date.getFullYear(), this.date.getMonth() - 1);
+            this.setDate(this.date);
+            this.partner.date = this.date;
+        }
+        )
+
+        rbt.addEventListener("click", function (event) {
+            var content = document.getElementById(this.divId);
+            var year_month_text = content.querySelector("div.datapicker").querySelector("div.year-month").querySelector("p.year-month-text");
+            year_month_text.textContent = "";
+
+            const elements = content.querySelector("div.datapicker").querySelector("div.grid-calendar").querySelectorAll(".grid-day");;
+            for (var i = 0; i < elements.length; i++) {
+                var elem = elements[i];
+                elem.parentNode.removeChild(elem);
+            }
+
+            const elementsDim = content.querySelector("div.datapicker").querySelector("div.grid-calendar").querySelectorAll(".grid-day-dimmed");
+            for (var i = 0; i < elementsDim.length; i++) {
+                var elem = elementsDim[i];
+                elem.parentNode.removeChild(elem);
+            }
+            this.date = new Date(this.date.getFullYear(), this.date.getMonth() + 1);
+            this.setDate(this.date);
+            this.partner.date = this.date;
+        }
+        )
+
 
     }
 }
